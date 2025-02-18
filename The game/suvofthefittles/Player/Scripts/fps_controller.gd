b3a937798b0cd83692164d1d469df68471fc643b
Sebastian Humm-Garø <sebastianhummgaro@icloud.com>
2025-02-18 09:54:01 +0100

    Uploded the current game

---
+++ The game/suvofthefittles/Player/Scripts/fps_controller.gd
@@ -0,0 +1,95 @@
+extends CharacterBody3D
+
+
+@export var SPEED : float = 5.0
+@export var JUMP_VELOCITY : float = 4.5
+
+@export var MOUSE_SENS : float = 0.3
+@export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
+@export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
+@export var CAMERA_CONTROLLER : Camera3D
+
+@export var Stamina = 10.0
+
+var _mouse_input : bool = false
+var _mouse_rotation : Vector3
+var _rotation_input : float
+var _tilt_input : float
+var _player_rotation : Vector3
+var _camera_rotation : Vector3
+
+#var sprint := Input.is_action_pressed("sprint")
+
+
+
+func _input(event):
+	if event.is_action_pressed("exit"):
+		get_tree().quit()
+
+func _unhandled_input(event):
+	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	if _mouse_input:
+		_rotation_input = -event.relative.x * MOUSE_SENS
+		_tilt_input = -event.relative.y * MOUSE_SENS
+		print(Vector2(_rotation_input,_tilt_input))
+
+func _update_camera(delta):
+	
+	#Rotate the cam
+	_mouse_rotation.x += _tilt_input * delta
+	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	_mouse_rotation.y += _rotation_input * delta
+	
+	_player_rotation = Vector3(0.0,_mouse_rotation.y,0.0)
+	_camera_rotation = Vector3(_mouse_rotation.x,0.0,0.0)
+	
+	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
+	CAMERA_CONTROLLER.rotation.z = 0.0
+	
+	global_transform.basis = Basis.from_euler(_player_rotation) 
+	
+	_rotation_input = 0.0
+	_tilt_input = 0.0
+	
+#func _stamina_bar(delta):
+	#var sprint = Input.is_action_pressed("sprint")
+	#if sprint and Stamina > 0:
+		#$Timer.start()
+		#
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	_update_camera(delta)
+	#_stamina_bar(delta)
+
+	# Handle jump.
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var sprint := Input.is_action_pressed("sprint")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction and sprint:
+		velocity.x = direction.x * SPEED * 2
+		velocity.z = direction.z * SPEED * 1.5
+	elif direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+		Stamina -= 1
+		print(Stamina)
